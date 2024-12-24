10f7b46c013113d7f2f00035c67bf2fd2869ce97
IsaacMuniz <im.isaac.muniz@gmail.com>
2024-12-24 09:34:10 -0300

    feat: learning queries syntax in graphql

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('Hello GraphQL!');
+import { ApolloServer, gql } from 'apollo-server';
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Query {
+      hello: String
+      hiThere: String
+    }
+  `,
+  resolvers: {
+    Query: {
+      hello: () => 'Hello, GraphQL!',
+      hiThere: () => 'Hi there!',
+    },
+  },
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
