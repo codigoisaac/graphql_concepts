e6caa04f866766ac5b2172c54eaf16b671adead7
IsaacMuniz <im.isaac.muniz@gmail.com>
2024-12-24 12:59:17 -0300

    feat: using params and better context for requests

---
+++ src/graphql/context.js
@@ -0,0 +1,8 @@
+import fetch from 'node-fetch';
+
+const API_URL = 'http://localhost:3000';
+
+export const context = () => ({
+  getUsers: (path = '') => fetch(`${API_URL}/users/${path}`),
+  getPosts: (path = '') => fetch(`${API_URL}/posts/${path}`),
+});
--- src/graphql/post/resolver.js
+++ src/graphql/post/resolver.js
@@ -1,9 +1,14 @@
-const post = () => ({ id: '1', title: 'test' });
+const post = async (_, { id }, { getPosts }) => {
+  const post = await getPosts(id);
 
-const posts = () => [
-  { id: '1', title: 'test' },
-  { id: '2', title: 'test1' },
-];
+  return post.json();
+};
+
+const posts = async (_, __, { getPosts }) => {
+  const posts = await getPosts();
+
+  return posts.json();
+};
 
 export const postResolvers = {
   Query: {
