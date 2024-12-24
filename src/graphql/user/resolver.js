4018c28e4f5c6ebe736e542c3d6bf3131354b769
IsaacMuniz <im.isaac.muniz@gmail.com>
2024-12-24 12:40:46 -0300

    feat: use params for fetch

--- src/graphql/user/resolver.js
+++ src/graphql/user/resolver.js
@@ -1,11 +1,11 @@
-const user = async (_, __, { fetch }) => {
-  const user = await fetch('http://localhost:3000/users/602');
+const user = async (_, { id }, { fetch }) => {
+  const user = await fetch(`http://localhost:3000/users/${id}`);
 
   return user.json();
 };
 
 const users = async (_, __, { fetch }) => {
-  const users = await fetch('http://localhost:3000/users');
+  const users = await fetch(`http://localhost:3000/users`);
 
   return users.json();
 };
